9f52da6b8ecd9dede17a073eb86dfd225be2959a
willa kiane <willa_abreu@outlook.com>
2023-10-16 22:56:55 -0300

    Update novos_metodos_planos.js

--- novos_metodos_planos.js
+++ novos_metodos_planos.js
@@ -78,7 +78,7 @@ function get_internets_validas() {
             var card = slide_dom_item.find('[data-id]')
             card.attr('data-id', internet.id)
             card.attr('data-nome-plano', internet.nome)
-            if(internet.nome && document.querySelector('[data-id="' +  internet.id + '"] [data-plano]')) { document.querySelector('[data-id="' +  internet.id + '"] [data-plano]').setAttribute('data-plano',  internet.nome.replace(/\D/g,'')) }
+            if(internet.nome) { $(document.querySelector('[data-id="' +  internet.id + '"] [data-plano]')).attr('data-plano',  internet.nome.replace(/\D/g,'')) }
             card.find('[data-titulo]').text(internet.nome)
             monta_preco(internet, ofertas, card)
         } else {
@@ -86,7 +86,7 @@ function get_internets_validas() {
             var card = clone.find('[data-id]')
             card.attr('data-id', internet.id)
             card.attr('data-nome-plano', internet.nome)
-            if(internet.nome && document.querySelector('[data-id="' +  internet.id + '"] [data-plano]')) { document.querySelector('[data-id="' +  internet.id + '"] [data-plano]').setAttribute('data-plano',  internet.nome.replace(/\D/g,'')) }
+            if(internet.nome) { $(document.querySelector('[data-id="' +  internet.id + '"] [data-plano]')).attr('data-plano',  internet.nome.replace(/\D/g,'')) }
             card.find('[data-melhor-oferta]').remove();
 
             card.find('[data-titulo]').text(internet.nome)
