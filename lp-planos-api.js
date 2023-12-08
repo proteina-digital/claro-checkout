a80f0fb220e629defd325c0faf363c2f87fdd1eb
eduardorph <eduardorph@hotmail.com>
2023-12-08 16:24:51 -0300

    Adicionada função de card combo

--- lp-planos-api.js
+++ lp-planos-api.js
@@ -145,6 +145,310 @@ function create_cards(tipo){
 }
 
 
+function create_cards_combo(){
+	var selecoes = JSON.parse(sessionStorage.getItem("selecoes"));
+	var ofertas = JSON.parse(sessionStorage.getItem("ofertas"));
+	var recursos = JSON.parse(sessionStorage.getItem("recursos"));
+	var planos_internet = JSON.parse(sessionStorage.getItem("planos_internet"));
+    var planos_tv = JSON.parse(sessionStorage.getItem("planos_tv"));
+    var planos_celular = JSON.parse(sessionStorage.getItem("planos_celular"));
+    var planos_fixo = JSON.parse(sessionStorage.getItem("planos_fixo"));
+
+	var preco_tv = 0;
+    var preco_internet = 0;
+    var preco_celular = 0;
+    var preco_fixo = 0;
+
+    var nome_tv = '';
+    var nome_internet = '';
+    var nome_cel = '';
+    var nome_tel = '';
+
+	var preco_final = 0;
+    var preco_oferta = 0;
+    var preco_boleto_final = 0;
+    var tempo_desconto = 0;
+    var preco_pos = 0;
+    var mensagem = "";
+    var url_params = "";
+    var url_plano = 'plano=COMBO-';
+
+    var data_card = "combos";
+
+    var titulo_combo = '';
+
+	
+
+	if( selecoes !== null ){
+
+		if( $('[data-cards="'+data_card+'"]').length > 0 ){
+			var data_cards = $('[data-cards="'+data_card+'"]');
+			var card_clonado = data_cards.find('.novo-slide').eq(0).clone();
+
+			// remove todos os cards desse tipo
+			data_cards.find('.novo-slide').remove();
+
+			var qtd_planos = selecoes.length;
+			var cc = 1;
+
+			selecoes.reverse();
+
+			for (var i = selecoes.length - 1; i >= 0; i--) {
+				var plano_combo = selecoes[i];
+
+				// if( '0_1181_182' !==  plano_combo.id){
+				// 	continue;
+				// }
+
+				card_clonado.removeAttr('aria-label');
+				card_clonado.attr('aria-label', cc+' of '+qtd_planos);
+				card_clonado.attr('data-card-id', plano_combo.id);
+
+
+				// INICIO TRATAMENTO DE COMBOS
+				var ids_combos = formata_combos(plano_combo.id);
+
+				var selecao_combo = selecoes.find(function(item) {
+                    return item.id === plano_combo.id;
+                });
+
+
+				// FORMATAR NOMES DE TV E/OU INTERNET
+                if( ids_combos[0] != 0 && ids_combos[0] !== undefined && ids_combos[1] != 0 && ids_combos[1] !== undefined ){
+                	var plano_tv = planos_tv.find(function(i) {
+	                    return i.id === ids_combos[0].toString();
+	                });
+
+
+	                var plano_internet = planos_internet.find(function(i) {
+	                    return i.id === ids_combos[1].toString();
+	                });
+
+                	titulo_combo = removerTermoDaPrimeiraString(plano_internet.nome, plano_tv.nome, 'Globoplay') +' + '+ plano_tv.nome; // INTERNET + TV
+
+                }else if( ids_combos[0] != 0 && ids_combos[1] != 1 ){
+
+                	var plano_tv = planos_tv.find(function(i) {
+	                    return i.id === ids_combos[0].toString();
+	                });
+
+                	titulo_combo = plano_tv.nome.nome; // TV
+
+                }else{
+                	var plano_internet = planos_internet.find(function(i) {
+	                    return i.id === ids_combos[1].toString();
+	                });
+
+                	titulo_combo = plano_internet.nome; // INTERNET
+
+                }
+
+
+                card_clonado.find(".novo-card-dropdown-list").find(".novo-dropdown-iitem").remove();
+
+
+				// TV
+				if(ids_combos[0] != 0 && ids_combos[0] !== undefined){
+                    ret_tv = preco_combo("tv", selecao_combo, planos_tv, ofertas);
+                    preco_tv = ret_tv[0];
+                    preco_tv_sdesconto = ret_tv[1];
+
+                    if(ret_tv[2] != 0){
+                        tempo_desconto = ret_tv[2];
+                    }else{
+                        preco_tv_sdesconto = preco_tv;
+                    }
+
+                    titulo_combo = titulo_combo;
+
+                    if( plano_tv.hasOwnProperty('recursosIds') ){
+
+						plano_tv.recursosIds.forEach(function(el) {
+							if( recursos[el] ){
+								card_clonado.find(".novo-card-dropdown-list").append('<div class="novo-dropdown-iitem"><strong>TV</strong>: '+recursos[el].descricao+'</div>')
+							}
+						});
+					}
+
+                }
+
+                // INTERNET
+                if(ids_combos[1] != 0 && ids_combos[1] !== undefined){
+                    ret_net = preco_combo("internet", selecao_combo, planos_internet, ofertas);
+                    preco_internet = ret_net[0];
+                    preco_internet_sdesconto = ret_net[1];
+
+                    if(ret_net[2] != 0){
+                        tempo_desconto = ret_net[2];
+                    }else{
+                        preco_internet_sdesconto = preco_internet;
+                    }
+
+                    titulo_combo = titulo_combo;
+
+                    if( plano_internet.hasOwnProperty('recursosIds') ){
+
+						plano_internet.recursosIds.forEach(function(el) {
+							if( recursos[el] ){
+								card_clonado.find(".novo-card-dropdown-list").append('<div class="novo-dropdown-iitem"><strong>INTERNET</strong>: '+recursos[el].descricao+'</div>')
+							}
+						});
+					}
+                }
+
+
+                // CELULAR
+                if(ids_combos[3] != 0 && ids_combos[3] !== undefined){
+                    ret_cel = preco_combo("celular", selecao_combo, planos_celular, ofertas);
+                    preco_celular = ret_cel[0];
+                    preco_cel_sdesconto = ret_cel[1];
+
+                    var plano_celular = planos_celular.find(function(i) {
+	                    return i.id === ids_combos[3];
+	                });
+
+                    titulo_combo += titulo_combo != '' ? ' + '+plano_celular.nome : plano_celular.nome;
+
+                    if(ret_cel[2] != 0){
+                        tempo_desconto = ret_cel[2];
+                    }else{
+                        preco_cel_sdesconto = preco_celular;
+                    }
+
+                    if( plano_celular.hasOwnProperty('recursosIds') ){
+
+						plano_celular.recursosIds.forEach(function(el) {
+							if( recursos[el] ){
+								card_clonado.find(".novo-card-dropdown-list").append('<div class="novo-dropdown-iitem"><strong>MÓVEL</strong>: '+recursos[el].descricao+'</div>')
+							}
+						});
+					}
+                }
+
+
+                // TELEFONE FIXO
+                if(ids_combos[2] != 0 && ids_combos[2] !== undefined){
+                    ret_tel = preco_combo("fone", selecao_combo, planos_fixo, ofertas);
+                    preco_fixo = ret_tel[0];
+                    preco_fixo_sdesconto = ret_tel[1];
+
+                    var plano_fixo = planos_fixo.find(function(i) {
+	                    return i.id === ids_combos[2];
+	                });
+
+                    titulo_combo += titulo_combo != '' ? ' + '+plano_fixo.nome : plano_fixo.nome;
+
+                    if(ret_tel[2] != 0){
+                        tempo_desconto = ret_tel[2];
+                    }else{
+                        preco_fixo_sdesconto = preco_fixo;
+                    }
+
+                    if( plano_fixo.hasOwnProperty('recursosIds') ){
+
+						plano_fixo.recursosIds.forEach(function(el) {
+							if( recursos[el] ){
+								card_clonado.find(".novo-card-dropdown-list").append('<div class="novo-dropdown-iitem"><strong>TEL</strong>: '+recursos[el].descricao+'</div>')
+							}
+						});
+					}
+                }
+
+
+   
+			    url_plano = url_plano+plano_combo.id;
+
+
+				card_clonado.find(".novo-card-titulo").text(titulo_combo.toUpperCase());
+				card_clonado.find(".novo-card-preco").text(formatarValor(preco_tv + preco_internet + preco_celular + preco_fixo));
+
+
+
+				if (location.search.length) {
+					url_params = location.search+"&";
+				}else{
+					url_params = '?';
+				}
+
+
+				var textos = textos_obs_planos(plano_combo.id, 'combo');
+
+				if( textos ){
+
+					if( card_clonado.find("[data-card-obs-destaque]").length > 0 ){
+						card_clonado.find("[data-card-obs-destaque]").text(textos);
+					}else{
+						card_clonado.find(".novos-cards-mascara").append('<p data-card-obs-destaque="" class="novo-card-chamada">'+textos+'</p>');
+					}
+				}else{
+					if( card_clonado.find("[data-card-obs-destaque]").length > 0 ){
+						card_clonado.find("[data-card-obs-destaque]").remove();
+					}
+				}
+
+				card_clonado.find(".novo-card-botao").attr('target', '_blank').attr('href', 'https://carrinho.ofertasclaro.com.br/'+url_params+url_plano);
+
+				// zerando novamente
+				url_plano = 'plano=COMBO-';
+
+
+
+				if( tempo_desconto > 0 ){
+                    if(tempo_desconto > 1){
+                        mensagem = "Nos "+tempo_desconto+" primeiros meses.";
+                    }else{
+                        mensagem = "No primeiro mês.";
+                    }
+
+                    var valor_final_sem_desconto = preco_tv_sdesconto + preco_internet_sdesconto + preco_cel_sdesconto + preco_fixo_sdesconto;
+
+                    card_clonado.find(".novo-card-area-preco").append('<p data-card-aviso-promo="" class="novo-card-preco-promocional">'+mensagem+'<br>Valor a partir do '+parseInt(tempo_desconto+1)+'º mês: '+formatarValor(valor_final_sem_desconto));
+                }else{
+                	card_clonado.find("[data-card-aviso-promo]").remove();
+                }
+
+
+				cc++;
+
+				data_cards.find(".novos-cards-mascara").append(card_clonado.clone());
+			}
+
+			var alturaMaxima = 0;
+		    data_cards.find(".novo-card-header").each(function() {
+		        alturaMaxima = Math.max(alturaMaxima, $(this).outerHeight());
+		    });
+
+		    // Atribuir a altura máxima a todos os filhos
+		    data_cards.find(".novo-card-header").css('height', alturaMaxima + 'px');
+
+			Webflow.require('slider').redraw();
+			Webflow.require('slider').ready();
+			Webflow.require('dropdown').ready();
+		}
+
+	}
+
+}
+
+
+function removerTermoDaPrimeiraString(string1, string2, termo) {
+  // Cria uma expressão regular com o termo e a flag 'i' para case-insensitive
+  const regex = new RegExp(termo, 'i');
+
+  // Testa se o termo existe em ambas as strings
+  const existeNaString1 = regex.test(string1);
+  const existeNaString2 = regex.test(string2);
+
+  var str1 = string1;
+
+  if( existeNaString1 && existeNaString2 ){
+  	str1 = string1.replace(regex, '');
+  }
+
+  return str1;
+}
+
+
 
 function textos_obs_planos(id, tipo){
 	var resultado = false;
